367e3d742676a77d85ff6e5bfaca8327b3735014
LesegoSE <lesego.seikali@gmail.com>
2023-10-07 14:49:41 -0700

    adding file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /*Displays a message*/
-int main (void)
+int main(void)
 {
 	const char *msg = "\"Programming is like building a multilingual puzzle";
-	puts (msg);
-	return (0);
+
+	puts(msg);
+	return(0);
 }
