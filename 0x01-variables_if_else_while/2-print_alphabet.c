9aa147ec2b83a179cfa4a0897777d4efb1005239
LesegoSE <lesego.seikali@gmail.com>
2023-10-09 12:55:24 -0700

    all caps aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (Success)
+*/
+int main(void)
+{
+	char t;
+
+	for (t = "a";t <= "z"; t++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
