34a4a16a34462399246862aa70b5e0285f2ce737
LesegoSE <lesego.seikali@gmail.com>
2023-10-09 13:07:20 -0700

    aphabets printer

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (Success)
+*/
+int main(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+		putchar(t);
+	}
+
+	for (t = 'A'; t <= 'Z'; t++)
+	{
+		putchar(t);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
