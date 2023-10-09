958fb54ae2091296b55f3a28554c1d03f4e00f6e
LesegoSE <lesego.seikali@gmail.com>
2023-10-09 14:01:42 -0700

    adding advanced file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int a;
+	int n;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+			for (n = '0'; n <= '9'; a++)
+			{
+				if (i < a && a < n)
+				{
+					putchar(i);
+					putchar(a);
+					putchar(n);
+
+					if (i != '7')
+					{
+						putchar(',');
+						putchar(',');
+					}
+				}
+			}
+		}
+	
+		putchar('\n');
+
+		return (0);
+	}
+
