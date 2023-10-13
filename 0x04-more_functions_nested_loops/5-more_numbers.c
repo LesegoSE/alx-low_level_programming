e5f759afdeacc559fd66856edb837b3c9d13861a
LesegoSE <lesego.seikali@gmail.com>
2023-10-13 15:17:04 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_numbers - print more numbers
+*
+*Return: void
+*/
+
+void more -numbers(void)
+{
+	int i;
+	int n;
+
+	for (i =1; i <= 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar('1');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
