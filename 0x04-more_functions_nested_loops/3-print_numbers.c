60d8ad9804fdf8aa2b1e1ae215b6fa8fe249118a
LesegoSE <lesego.seikali@gmail.com>
2023-10-13 14:10:51 -0700

    print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - prints numbers from 0 to 9
+*
+*Return: Always 0.
+*/
+void print numbers(void)
+{
+	int i = 0;
+
+	do while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	_putchar ("\n");
+}
