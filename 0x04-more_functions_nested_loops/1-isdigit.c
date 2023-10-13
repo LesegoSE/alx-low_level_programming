8ac9da7b12232179e02fbc5b66edc4f17c016c00
LesegoSE <lesego.seikali@gmail.com>
2023-10-13 13:29:16 -0700

    check if digit is integer

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - checks if variable is intiger returns 0 if not and 1 if id
+ *@c: varibel to be checked
+ *Return: 0 if false 1 if true
+*/
+int _isdigit(int c)
+{
+	if (isdigit(c) = 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
