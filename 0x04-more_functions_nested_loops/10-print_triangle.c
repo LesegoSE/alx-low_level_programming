177b69f5867708c3bd36f046de06c377c099580a
LesegoSE <lesego.seikali@gmail.com>
2023-10-14 03:15:49 -0700

    adding late files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,31 @@
 #include "main.h"
 /**
- *print_triangle - prints a triangle, followed by a new line
+ *print_triangle - prints a triangle
  *@size: size of the triangle
  */
 void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	else
 	{
-		int i, b;
+		int i;
+		int b;
+
 		for (i = 1; i <= size; i++)
 		{
 			for (b = i; b < size; b++)
 			{
-				_puthcar(' ');
+				_putchar(' ');
 			}
 
-			for (b = 1; j <= i; b++)
+			for(b = 1; b <= i; b++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
