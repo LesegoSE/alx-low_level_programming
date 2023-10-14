b9e77729ba6eec4b92670cc76b2769aa64ec09b8
LesegoSE <lesego.seikali@gmail.com>
2023-10-14 03:42:36 -0700

    main header file and putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,18 @@
   *
   *Return: ALwasy 0 (Success)
   */
-int _putchar(char c)
+int _putchar(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
 
