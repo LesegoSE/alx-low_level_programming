b87741519bde85cbd2f0771281601439c954bf0c
LesegoSE <lesego.seikali@gmail.com>
2023-10-14 03:53:40 -0700

    printletters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(l);
+	}
+
+	_putchar('\n');
+}
