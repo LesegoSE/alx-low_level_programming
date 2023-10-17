deb5814c34629ee8678b37fadfa24e4f75f5d21c
LesegoSE <lesego.seikali@gmail.com>
2023-10-17 13:10:49 -0700

    printer file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints the string followed by a new line
+ * @str: pointer variable holding address of string to be displayed
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("/n");
+}
