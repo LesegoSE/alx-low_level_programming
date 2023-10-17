2006ba0ab91b5364eac724e2c259c58fdcd1785d
LesegoSE <lesego.seikali@gmail.com>
2023-10-17 10:58:19 -0700

    change varibles value via pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
  * reset_to_98 - update the value of a varibale the pointer points to.
- *
+ * @*n: variable holding pointer value for variable that is to be updated.
  * return: The updated variable value.
  */
 
