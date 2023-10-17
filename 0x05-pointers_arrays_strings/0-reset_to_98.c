44fe31f9793ce882b5878c6536283c46a21957e1
LesegoSE <lesego.seikali@gmail.com>
2023-10-17 10:51:45 -0700

    change varibles value via pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a varibale the pointer points to.
+ *
+ * return: The updated variable value.
+ */
+
+int reset_to_98(int *c)
+{
+	*c = 98;
+
+	return (c);
+}
