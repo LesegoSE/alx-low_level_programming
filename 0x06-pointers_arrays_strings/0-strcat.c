55ba07870a57dd0c6825decaa9a8f648006f5b2f
LesegoSE <lesego.seikali@gmail.com>
2023-10-19 10:36:52 -0700

    concater

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates text in variables named dest and src
+ * @dest: pointer passing string to be concatenated.
+ * @src: pointer passing string to be concatenated.
+ * Return: concatenated string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int i;
+
+	n = 0;
+	i = 0;
+	while(dest[n] != '\0')
+	{
+		n++;
+	}
+
+	while(src[i] != '\0')
+	{
+		dest[n] = src[i];
+		n++;
+	}
+
+	dest[n] = '\0';
+	return (dest);
+}
