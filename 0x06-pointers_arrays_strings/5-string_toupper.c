3a345005f369d97ab1765eff9dfcb9a69e253bdd
LesegoSE <lesego.seikali@gmail.com>
2023-10-20 10:24:39 -0700

    Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,11 +12,11 @@ char *string_toupper(char *b)
 	int i;
 
 	i = 0;
-	while((*(b + i)) != '\0')
+	while(*(b + i) != '\0')
 	{
-		if((*(b + i)) >= 'a' && (*(b + i) <= 'z)')
+		if(*(b + i) >= 'a' && *(b + i) <= 'z')
 		{
-			(*(b + i)) = ((*(b + i)) - 32);
+			*(b + i) = (*(b + i) - 32);
 		}
 		i++;
 	}
