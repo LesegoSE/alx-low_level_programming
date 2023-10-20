c77852cbd501f1dd1d086499462bbe05c8580e3c
LesegoSE <lesego.seikali@gmail.com>
2023-10-20 12:31:14 -0700

    num print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,25 +2,50 @@
 
 /**
  * print_number - a function that prints an integer.
- * @n: parameter variable
- *
- * Return: 0 Always Success
+ * @n parameter variable
+ * 
+ * Return: void
  */
 
 void print_number(int n)
 {
-	unsigned int b;
+	int c;
+	int b;
+	int di;
+	int d;
+	int power;
+	unsigned int temp;
+	unsigned int numch;
+	unsigned int num;
 
-	0 = b;
-	b = n;
+	di = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		b = -n;
-	}
-	if (b / 10 != 0)
+		temp = -n;
+	} else
 	{
-		print_number(b / 10);
+		temp = n;
 	}
-	_putchar((b % 10) + '0');
+	num = temp;
+	while (num >= 10)
+	{
+		num = num / 10;
+		di++;
+	}
+	d = di + 1;
+	power = 1;
+	c = 1;
+	while (c < d)
+	{
+		ex = ex * 10;
+		c++;
+	}
+	b = ex;
+	while (b >= 1)
+	{
+		numch = (temp / b) % 10;
+		_putchar(numch + '0');
+		b = b / 10;
+	}
 }
