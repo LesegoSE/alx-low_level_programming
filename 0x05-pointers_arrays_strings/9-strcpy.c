798326e2a92406056137aff2c9eedb64ce871f76
LesegoSE <lesego.seikali@gmail.com>
2023-10-21 03:13:45 -0700

    n

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0)
+ * @dest: parameter pointer
+ * @src: parrameter pointer
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *og_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return(og_dest);
+}
