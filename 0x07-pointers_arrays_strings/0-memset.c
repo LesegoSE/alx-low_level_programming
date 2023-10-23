82630bdce924f5836a789cbb47dbbcd5b31da5e1
LesegoSE <lesego.seikali@gmail.com>
2023-10-23 10:57:15 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int cnt;
-	int c;
 
-	c = n;
-
-	for (cnt = 0; cnt < c; cnt++)
+	for (cnt = 0; cnt < n; cnt++)
 	{
 		s[cnt] = b;
-		n--;
+		return (s);
 	}
-	return (s);
 }
