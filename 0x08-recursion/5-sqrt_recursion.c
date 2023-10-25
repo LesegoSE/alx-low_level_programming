11a4d75bdac628c0cdfcc0b33351498fd36c891a
LesegoSE <lesego.seikali@gmail.com>
2023-10-25 12:18:33 -0700

    rec

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rec - Iterates through 1 to n value
+ * @i: prameter variable
+ * @c: prameter variable
+ * Return: int
+ */
+
+int rec(int i, int c);
+
+/**
+ * is_prime_number - a function that returns the natural square root of a number.
+ * @n: prameter variable
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime2(n, 2));
+}
+
+int rec(int i, int c)
+{
+	if (i == c)
+	{
+		return (1);
+	} else if (i % c == 0)
+	{
+		return (0);
+	}
+	return (prime2(i, c + 1));
+}
