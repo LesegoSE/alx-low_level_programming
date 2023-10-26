0cbfab39795087816510cd617ab82adbf1e9c6b1
LesegoSE <lesego.seikali@gmail.com>
2023-10-26 11:53:13 -0700

    cube

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,7 +19,8 @@ int cube(int i, int c)
 return (cube(i, c + 1));
 }
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns the natural
+ * square root of a number.
  * @n: parameter variable
  * Return: square root number
  */
