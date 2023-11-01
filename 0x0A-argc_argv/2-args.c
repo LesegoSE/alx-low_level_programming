77f1c0d801756f11b659c41d45f792e5b40e6c38
LesegoSE <lesego.seikali@gmail.com>
2023-11-01 13:57:52 -0700

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc - counts the number of args passed.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 Always success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
