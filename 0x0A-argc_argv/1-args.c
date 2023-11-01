9a2f37464d262d494e1180f726243b12568690a8
LesegoSE <lesego.seikali@gmail.com>
2023-11-01 13:48:21 -0700

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints the number of arguments passed into it.
- * @argc - counts the number of args passed.
+ * @argc: counts the number of args passed.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d/n", argc);
+	printf("%d/n", argc - 1);
 	return (0);
 }
