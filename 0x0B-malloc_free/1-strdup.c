e53ec23e64b8a75db95589ecf961c9dce73564d2
LesegoSE <lesego.seikali@gmail.com>
2023-11-01 15:32:33 -0700

    maloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	char *ou;
+	unsigned int n;
+	unsigned int c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		ou = (char *)malloc(sizeof(char) * (n + 1));
+	}
+	if (ou == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c <= n; c++)
+	{
+		ou[c] = str[c];
+	}
+	return (ou);
+}
