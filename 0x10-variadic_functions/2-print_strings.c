843130d23de5a1856b77a1dd1f4121d57f751407
LesegoSE <Lesego.seikali@gmail.com>
2023-11-18 21:44:25 -0800

    l

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char *);
+		c = va_arg(strings, char *);
 		if (c == NULL)
 		{
 			printf("(nil)");
